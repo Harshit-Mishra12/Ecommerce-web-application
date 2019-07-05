1a80405f4833aa624c13dcc8fdd5a2b6ca86cc69
161b292 <harshitmishra5348@gmail.com>
2019-07-05 10:39:31 +0530

    shopping cart view error solved and checkout is added

--- routes/index.js
+++ routes/index.js
@@ -35,29 +35,29 @@ router.get("/",function(req,res){
 
 });
 router.post("/check",function(req,res){
-      console.log("oooooooooooooooo");
-       console.log(req.body.brand);
+      // console.log("oooooooooooooooo");
+      //  console.log(req.body.brand);
 
-      console.log(req.body.title);
+      // console.log(req.body.title);
       var arr=req.body.brand;
       var arr2=req.body.title;
-   console.log(arr[0]);   
- console.log(arr2[0]);
+ //   console.log(arr[0]);   
+ // console.log(arr2[0]);
      var flag=0;
      var temp=0;
 
-    console.log(flag);
+    // console.log(flag);
     	if(arr[0]==null){
-    		console.log("flag=1 hia kya")
+    		// console.log("flag=1 hia kya")
     	 flag=1;	
     }
 
     
      	if ( arr2[0]==null) {
-     		console.log("temp=2 hia kya");
+     		// console.log("temp=2 hia kya");
      		 temp=2;
      	}
-    	if (flag==0) {console.log("flag ki value kya hai");}
+    	// if (flag==0) {console.log("flag ki value kya hai");}
     
 
 
@@ -65,7 +65,7 @@ router.post("/check",function(req,res){
 	  
       if(flag==0  && temp==2)
       {
-      	console.log("yahan pahuche??part1");
+      	// console.log("yahan pahuche??part1");
 		 Product.find({brand:req.body.brand},function(err,allCampgrounds){
       // JSON.stringify(allCampgrounds);
   
@@ -74,7 +74,7 @@ router.post("/check",function(req,res){
 		}
 		else{
 			// console.log(allCampgrounds); 
-			console.log("yeh kya backchodi hai");
+			// console.log("yeh kya backchodi hai");
 		 res.render("shop/index2.ejs",{campgrounds:allCampgrounds,successMsg:successMsg,noMessages:!successMsg}); 
 		    // res.send(allCampgrounds);
 
@@ -85,8 +85,9 @@ router.post("/check",function(req,res){
 		}
 
 		 if(flag==1  && temp==0)
-      {console.log("yahan pahuche??part2")
-  console.log(req.body.title);
+      {
+  //     	console.log("yahan pahuche??part2")
+  // console.log(req.body.title);
 		 Product.find({title:req.body.title},function(err,allCampgrounds){
       
   
@@ -100,7 +101,7 @@ router.post("/check",function(req,res){
 			});
 		}
 		if(flag==0 && temp==0){
-			console.log("yahan pahuche??")
+			// console.log("yahan pahuche??")
 			Product.find({brand:req.body.brand,title:req.body.title},function(err,allCampgrounds){
       
   
@@ -108,7 +109,7 @@ router.post("/check",function(req,res){
 		    console.log(err);
 		}
 		else{
-			console.log(allCampgrounds);
+			// console.log(allCampgrounds);
 			if(allCampgrounds[0]==null){
 				  console.log("ismye kuch nahin hai");
 		   res.render("shop/index3.ejs");
@@ -122,8 +123,10 @@ router.post("/check",function(req,res){
 
 		}
 			 if(flag==1  && temp==2)
-      {console.log("yahan pahuche??part3")
-  console.log(req.body.title);
+      {
+
+    //   	console.log("yahan pahuche??part3")
+  		// console.log(req.body.title);
 		 Product.find({},function(err,allCampgrounds){
       
   
@@ -143,7 +146,7 @@ router.post("/check",function(req,res){
 
 
 router.put('/add-to-cart/:id',function(req,res,next){
-     console.log("add to cart nahin huha kya");
+     // console.log("add to cart nahin huha kya");
     var productId=req.params.id;
     console.log(productId);
     var cart=new Cart(req.session.cart ? req.session.cart:{});
@@ -156,9 +159,9 @@ router.put('/add-to-cart/:id',function(req,res,next){
          }
          cart.add(product, product.id);
          req.session.cart=cart;
-         console.log(req.session.cart);
-        console.log("total check kro bhaiya ji");
-        console.log(cart.totalQty);
+        //  console.log(req.session.cart);
+        // console.log("total check kro bhaiya ji");
+        // console.log(cart.totalQty);
          // res.redirect('/check');
           res.send(cart.totalQty+"");
 
@@ -166,7 +169,7 @@ router.put('/add-to-cart/:id',function(req,res,next){
 });
 router.get('/shopping-cart',function(req,res,next){
    if(!req.user){
-		console.log("baaaaaaaaad");
+		// console.log("baaaaaaaaad");
 	}
     
 
@@ -179,6 +182,17 @@ router.get('/shopping-cart',function(req,res,next){
    res.render('shop/shopping-cart.ejs',{products:cart.generateArray(),totalPrice:cart.totalPrice,checkUser:req.user});
 
 });
+router.get('/checkout',function(req,res,next){
+   
+   if (!req.session.cart) {
+   	return res.redirect('shop/shopping-cart.ejs');
+   }    
+   var cart = new Cart(req.session.cart);
+   var errMsg=req.flash('error')[0];
+   res.render('shop/checkout.ejs',{total:cart.totalPrice,errMsg:errMsg,noError:!errMsg});
+
+});
+
 
 
 module.exports=router;
